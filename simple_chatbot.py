d6aca9695a9785a41e881ce236c8dfbca05339e2
MargaretLai <MargaretLUI01@gmail.com>
2024-07-07 13:31:01 -0400

    Added code to make conversation interative

--- simple_chatbot.py
+++ simple_chatbot.py
@@ -2,26 +2,39 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
+# Variables
 load_dotenv()
-
 client = OpenAI(
     api_key = os.getenv("OPENAI_API_KEY")
 )
+messages = []
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system", 
-            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
-        },
-        {
-            "role": "user", 
-            "content": "Compose a poem that explains the concept of recursion in programming."
-        }
-    ],
-    temperature=0.7,
-    max_token=64
-)
+# Work flow
+print("------ Conversation Started ------\n")
+print("****** Say bye to end the conversation *****\n")
 
-print(completion.choices[0].message.content)
+user_input = input("What can I help you with?\n")
+
+while user_input.lower() != "bye":  # Keep conversation going as long as user doesn't say bye
+    messages.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.7,
+        max_tokens = 150
+    )
+
+    print(completion.choices[0].message.content)
+
+    messages.append({
+        "role": "system",
+        "content": completion.choices[0].message.content
+    })
+
+    user_input = input()
+
+print("\n------ Conversation Ended ------")
